aaebc0b1f6548089bbd0442728f8ea5778e99c95
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2024-09-09 23:44:29 +0100

    ast: Pretty printing for Expression

--- src/ast.rs
+++ src/ast.rs
@@ -1,14 +1,14 @@
 use crate::lexer::*;
 use display_tree::{format_tree, DisplayTree, StyleBuilder};
-use std::collections::VecDeque;
 use std::fmt;
 use std::mem::discriminant;
+use std::{collections::VecDeque, hash::Hash};
 use strum_macros::{Display, EnumIs};
 use thiserror::Error;
 
 static INDENT: &str = "  ";
 static CON_VERT: &str = "│";
-static CON_LINE: &str = "─";
+// static CON_LINE: &str = "──";
 static CON_MID: &str = "├─";
 static CON_END: &str = "└─";
 
@@ -146,12 +146,20 @@ impl FunctionDeclaration {
             .take(indent_level)
             .collect::<String>();
 
-        writeln!(f, "{}FunctionDeclaration", indent)?;
-        writeln!(f, "{}{} ({:?})", indent, self.name, self.params)?;
-        writeln!(f, "{}{}", indent, self.return_type)?;
-        if let Some(body) = &self.body {
-            body.tree_print(f, indent_level + 1)?;
+        writeln!(f, "{indent}FunctionDeclaration")?;
+        writeln!(f, "{indent}{CON_MID}name: {}", self.name)?;
+        if !self.params.is_empty() {
+            writeln!(f, "{indent}{CON_MID}params:")?;
+            for param in &self.params {
+                writeln!(f, "{indent}{CON_VERT} {CON_MID}{}", param)?;
+            }
         }
+        writeln!(f, "{indent}{CON_MID}type: {}", self.return_type)?;
+        if let Some(body) = &self.body {
+            writeln!(f, "{indent}{CON_END}body:")?;
+            body.tree_print(f, indent_level + 2)?;
+        }
+        writeln!(f, "")?;
         Ok(())
     }
 }
@@ -371,9 +379,12 @@ impl VariableDeclaration {
             .take(indent_level)
             .collect::<String>();
 
-        writeln!(f, "{}VariableDeclaration(int, {})", indent, self.name)?;
+        writeln!(f, "{indent}VariableDeclaration")?;
+        writeln!(f, "{indent}{CON_MID}type: {}", "int")?;
+        writeln!(f, "{indent}{CON_MID}name: {}", self.name)?;
         if let Some(init) = &self.init {
-            init.tree_print(f, indent_level + 1)?;
+            writeln!(f, "{indent}{CON_END}init:")?;
+            init.tree_print(f, indent_level + 2)?;
         }
         Ok(())
     }
@@ -471,12 +482,15 @@ impl Statement {
             }
             Statement::For(init, cond, post, body, label) => {
                 writeln!(f, "{}For [{}]", indent, option_ident_to_string(label))?;
-                writeln!(f, "{}{CON_MID}init: {}", indent, init)?;
+                writeln!(f, "{}{CON_MID}init:", indent)?;
+                init.tree_print(f, indent_level + 2)?;
                 if let Some(cond) = cond {
-                    writeln!(f, "{}{CON_MID}cond: {}", indent, cond)?;
+                    writeln!(f, "{}{CON_MID}cond:", indent)?;
+                    cond.tree_print(f, indent_level + 2)?;
                 }
                 if let Some(post) = post {
-                    writeln!(f, "{}{CON_MID}post: {}", indent, post)?;
+                    writeln!(f, "{}{CON_MID}post:", indent)?;
+                    post.tree_print(f, indent_level + 2)?;
                 }
                 writeln!(f, "{}{CON_END}body:", indent)?;
                 body.tree_print(f, indent_level + 1)?;
@@ -802,6 +816,28 @@ impl fmt::Display for ForInit {
     }
 }
 
+impl ForInit {
+    pub fn tree_print(&self, f: &mut fmt::Formatter, indent_level: usize) -> fmt::Result {
+        let indent = std::iter::repeat(INDENT)
+            .take(indent_level)
+            .collect::<String>();
+
+        match self {
+            ForInit::InitNull => writeln!(f, "{indent};")?,
+            ForInit::InitDecl(decl) => {
+                writeln!(f, "{indent}InitDecl")?;
+                decl.tree_print(f, indent_level + 1)?;
+            }
+            ForInit::InitExp(exp) => {
+                writeln!(f, "{indent}InitExp")?;
+                exp.tree_print(f, indent_level + 1)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug, PartialEq, Clone, DisplayTree)]
 #[allow(dead_code)]
 pub enum Expression {
@@ -933,12 +969,11 @@ impl Expression {
             .collect::<String>();
 
         match self {
-            Expression::Assignment(left, right) => {
-                writeln!(f, "{indent}Assignment")?;
-                writeln!(f, "{indent}{CON_MID}Left")?;
-                left.tree_print(f, indent_level + 2)?;
-                writeln!(f, "{indent}{CON_END}Right")?;
-                right.tree_print(f, indent_level + 2)?;
+            Expression::Unary(op, val) => {
+                writeln!(f, "{indent}Unary")?;
+                writeln!(f, "{indent}{CON_MID}operator: {}", op)?;
+                writeln!(f, "{indent}{CON_MID}Value")?;
+                val.tree_print(f, indent_level + 2)?;
             }
             Expression::Binary(op, left, right) => {
                 writeln!(f, "{indent}Binary")?;
@@ -947,6 +982,31 @@ impl Expression {
                 left.tree_print(f, indent_level + 2)?;
                 writeln!(f, "{indent}{CON_END}Right")?;
                 right.tree_print(f, indent_level + 2)?;
+            }
+            Expression::Assignment(left, right) => {
+                writeln!(f, "{indent}Assignment")?;
+                writeln!(f, "{indent}{CON_MID}Left")?;
+                left.tree_print(f, indent_level + 2)?;
+                writeln!(f, "{indent}{CON_END}Right")?;
+                right.tree_print(f, indent_level + 2)?;
+            }
+            Expression::Conditional(cond, then_exp, else_exp) => {
+                writeln!(f, "{indent}Conditional")?;
+                writeln!(f, "{indent}{CON_MID}Cond")?;
+                cond.tree_print(f, indent_level + 2)?;
+                writeln!(f, "{indent}{CON_MID}Then")?;
+                then_exp.tree_print(f, indent_level + 2)?;
+                writeln!(f, "{indent}{CON_END}Else")?;
+                else_exp.tree_print(f, indent_level + 2)?;
+            }
+            Expression::FunctionCall(name, args) => {
+                writeln!(f, "{indent}FunctionCall")?;
+                writeln!(f, "{indent}{CON_MID}Name")?;
+                writeln!(f, "{indent}{CON_VERT} {CON_END}{}", name)?;
+                writeln!(f, "{indent}{CON_END}Args")?;
+                for arg in args {
+                    arg.tree_print(f, indent_level + 2)?;
+                }
             }
             _ => writeln!(f, "{}{}", indent, self)?,
         }
